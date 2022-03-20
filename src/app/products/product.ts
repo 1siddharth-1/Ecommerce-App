dc62022b5395eafca0f2ca3915729b7884670805
Siddharth Singh Chouhan <42543548+1siddharth-1@users.noreply.github.com>
2022-03-21 03:12:40 +0530

    Add files via upload

---
+++ src/app/products/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    produceId: string;
+    categoryId: number;
+    productName: string;
+    descriptions: string;
+    rating: string;
+    price: number;
+    productImg: string;
+    isAvailable: boolean;
+    color: string;
+    review: number;
+}
